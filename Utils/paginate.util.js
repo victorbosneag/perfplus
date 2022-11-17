d93bc1523ec0c2f5c0780152352ab7192ef1eca8
victorbosneag <victor.bosneag@hotmail.com>
2022-11-17 09:16:36 +0200

    added pagination functions

--- Utils/paginate.util.js
+++ Utils/paginate.util.js
@@ -6,5 +6,67 @@ Accepts:
 - pageSize: number of items on page
 - search: search queries
 - order: order array for sequelize query
-- transform: fn to be applied to all rows
+- transform: fn to be applied to all row
 */
+
+export const paginate = async (model, pagenNr, pageSize, pageMax, search={}, order=[], transform) => {
+    try{
+        const page = parseInt(pageNr, 10) || 10;
+        const maxSize = parseInt(pageMax, 10) || 30;
+        const size = parseInt(pageSize, 10) || 10 > maxSize
+            ? maxSize
+            : parseInt(pageSize, 10) || 10;
+        let options = {
+            offset: findOffset(page, size),
+            limit: size
+        }
+        if (Object.keys(search).length) {
+            options = {options, ...search};
+        }
+
+        
+        if (order && order.length) {
+            options['order'] = order;
+        }
+
+        
+        let {count, rows} = await model.findAndCountAll(options);
+        if (transform && typeof transform === 'function') {
+            rows = transform(rows);
+        }
+
+        return {
+            previousPage: getPreviousPage(page),
+            currentPage: page,
+            nextPage: getNextPage(page, size, count),
+            total: count,
+            limit: limit,
+            data: rows
+        }
+        
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+    
+}
+
+const findOffset = (page, size) => {
+    return (page * size) - size;
+   }
+   
+const getNextPage = (page, limit, total) => {
+    if ((total/limit) > page) {
+        return page + 1;
+    }
+
+    return null
+}
+
+const getPreviousPage = (page) => {
+    if (page <= 1) {
+        return null
+    }
+    return page - 1;
+}
