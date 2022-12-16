50df76dc7df2d2a24405a8abee55aa85c45a4546
victorbosneag <victor.bosneag@hotmail.com>
2022-12-16 20:55:10 +0200

    bug fix

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -7,11 +7,13 @@ const User = db.users;
 export default async (req,res,next)=>{
     try {
         const token = req.body.token;
+        delete req.body.token;
         if(!token){
             return next('Please login to access the data');
         }
         const verify = await jwt.verify(token,process.env.SECRET_KEY);
-        req.user = await User.findById(verify.id);
+        console.log(verify);
+        req.user = await User.findOne({where:{id:verify.id}});
         next();
     } catch (error) {
        return next(error); 
